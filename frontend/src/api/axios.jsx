677d086841418ac914ef6f8e4d183cfbc2956128
aceto98 <a.jovanov098@yahoo.com>
2026-09-14 20:03:47 +0200

    Add dockerfiles and docker-compose

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import useStore from "../store";
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+const apiBaseUrl = "/api";
 
 const api = axios.create({
   baseURL: apiBaseUrl,
